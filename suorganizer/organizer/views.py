f3c40b98ff719b7a9ab9471dfd1470e5dad63e8d
yuebei <yuebei58@gmail.com>
2018-08-15 18:31:57 +0800

    #2018-8-15, upload

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
 
-# Create your views here.
+from .models import Tag
+
+def homepage(request):
+    tag_list = Tag.objects.all()
+    output = ", ".join([tag.name for tag in tag_list])
+    return HttpResponse(output)
