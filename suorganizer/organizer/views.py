1dc09e583b1b4f7a40beefa2ae09f5ae9720c4b8
yuebei <yuebei58@gmail.com>
2018-08-17 18:57:27 +0800

    #2018-8-17, upload

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -1,8 +1,16 @@
-from django.http.response import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
 from .models import Tag
 
 def homepage(request):
-    tag_list = Tag.objects.all()
-    output = ", ".join([tag.name for tag in tag_list])
-    return HttpResponse(output)
+    return render(
+        request,
+        'organizer/tag_list.html', 
+        {'tag_list':Tag.objects.all()})
+
+def tag_detail(request, slug):
+    tag = get_object_or_404(Tag, slug__iexact=slug)
+    return render(
+        request,
+        'organizer/tag_detail.html',
+        {'tag':tag})
